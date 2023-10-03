45ffa77a32be52be56876cfd560372d12a0b830e
Ericson-Manalo <81231219+Ericson-Manalo@users.noreply.github.com>
2023-10-03 11:48:15 +0200

    Initialize create-branch

---
+++ resources/views/plates/create.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+
+    <title>VUOTO</title>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="/CSS/create.css" rel="stylesheet">
+</head>
+
+<body class="p-0 m-0 border-0 bd-example m-0 border-0">
+
+</body>
+
+</html>
