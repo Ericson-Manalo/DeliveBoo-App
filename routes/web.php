da5a0f970041701e8fc343265f27f204c261abef
Ericson-Manalo <81231219+Ericson-Manalo@users.noreply.github.com>
2023-10-03 11:48:19 +0200

    Initialize styling-index-plate

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PlateController as PlateController;
+use App\Http\Controllers\OrderController as OrderController;
+use App\Http\Controllers\BraintreeController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// ----------   PLATE'S ROUTES -------- //
+Route::middleware('auth')->group( function() {
+    Route::resource('/plates', PlateController::class);
+});
+
+Route::get('orders', [OrderController::class, 'index'])->name('orders.index');
+
+Route::any('/payment', [BraintreeController::class, 'token'])->name('token')->middleware('auth');
