d3b33813bcc966dd49ca807f2773bcd6ae3fbaeb
Ericson-Manalo <81231219+Ericson-Manalo@users.noreply.github.com>
2023-10-03 11:48:16 +0200

    Initialize Backoffice-User-Front-End-updated

---
+++ app/Models/Restaurant.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Restaurant extends Model
+{
+    use HasFactory;
+
+    // protected $fillable = [
+    //     'user_id',
+    //     'name',
+    //     'address',
+    //     'VAT_number',
+    //     'email',
+    //     'opening_time',
+    //     'image',
+    //     'telephone_number',
+    //     'vote',
+    // ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+   
+    public function typologies(){
+        return $this->belongsToMany(Typology::class);
+    }
+
+    public function plates(){
+        return $this->hasMany(Plate::class);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+//<<<<<<< HEAD
+use App\Http\Controllers\Api\RestaurantController;
+//=======
+use App\Http\Controllers\Api\PlateController as ApiPlateController;
+use App\Http\Controllers\Api\TypologyController;
+
+//>>>>>>> plate-crud
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/typology',[TypologyController::class, 'index' ])->name('api.typology.index');
+
+//<<<<<<< HEAD
+Route::get('/restaurant', [ RestaurantController::class, 'index' ])->name('api.restaurants.index');
+//=======
+Route::get('/plates', [ ApiPlateController::class, 'index' ])->name('api.plates.index');
+
+Route::get('/plates/{plate}', [  ApiPlateController::class, 'show' ])->name('api.plates.show');
+
+// <<<<<<< HEAD
+// <<<<<<< HEAD
+// Route::post('/plates/create', [ ApiPlateController::class, 'store'])->name('api.plates.create');
+// //>>>>>>> plate-crud
+// =======
+
+// >>>>>>> myOrders
+// =======
+// // <<<<<<< HEAD
+// // Route::post('/plates/create', [ ApiPlateController::class, 'store'])->name('api.plates.create');
+// // //>>>>>>> plate-crud
+// // =======
+
+// // >>>>>>> plate-crud
+// >>>>>>> e6f09e79b3fd34660babea12c215bd03b80a1f69
