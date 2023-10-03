8a1548d67f65dbac329491bcd5f56e1b226737a4
Ericson-Manalo <81231219+Ericson-Manalo@users.noreply.github.com>
2023-10-03 11:48:17 +0200

    Initialize restaurant_typology-relationship

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PlateController as PlateController;
+use App\Http\Controllers\OrderController as OrderController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// ----------   PLATE'S ROUTES -------- //
+Route::middleware('auth')->group( function() {
+    Route::resource('/plates', PlateController::class);
+});
+
+Route::get('orders', [OrderController::class, 'index'])->name('index');
